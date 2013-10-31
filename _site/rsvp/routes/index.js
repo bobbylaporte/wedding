4b642389a68ae71a71d8ed1d3e731d1fa3e48c99
Bobby LaPorte <robertlaporte@gmail.com>
2013-10-30 22:48:41 -0500

    stop motion

---
+++ _site/rsvp/routes/index.js
@@ -0,0 +1,50 @@
+
+/*
+ * SUBMIT RSVP
+ */
+ exports.index = function(req, res){
+   res.send('RSVP App: Post to submit method on port 8888');
+ };
+
+
+ exports.submit = function(req, res){
+   //res.send(req);
+   console.log(req.body);
+   //res.send('Response From Place');
+   //Currently we rename the file based on the title. Every Time.
+   /*var title = req.body.recipe.title;
+   var datetime = req.body.recipe.datetime;
+   var categoriesArray = req.body.recipe.categories;
+   var ingredientsArray = req.body.recipe.ingredients;
+   var directionsArray = req.body.recipe.directions;
+
+   var date = datetime.substring(0, 10);
+   var name = title.toLowerCase().replace(/[^\w\s]/gi, '').replace(/\ /g,'-');
+
+
+   var md  = '---\n';
+       md += 'layout: post\n';
+       md += 'title: '+ title +'\n';
+       md += 'date: '+ datetime +'\n';
+       md += 'categories:';
+       categoriesArray.forEach(function(category){
+         md += ' ' + category;
+       });
+       md +='\n';
+       md += '---';
+       md += '\n';
+       md += '{% contentfor ingredients %}\n';
+       ingredientsArray.forEach(function(ingredient){
+         md += '- ' + ingredient + '\n';
+       });
+       md += '{% endcontentfor %}\n';
+       md += '\n';
+       md += '{% contentfor directions %}\n';
+       var i = 1;
+       directionsArray.forEach(function(direction){
+         md += i + '. ' +  direction + '\n';
+         i = i + 1;
+       });
+   */
+   //res.redirect("/");
+ };
